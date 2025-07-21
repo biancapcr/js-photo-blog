890cb5cd7c253882b5f432a0474e32de3ddda0f3
biancapcr <biancacristina.pocora@gmail.com>
2025-07-21 19:53:18 +0200

    [add] last part of js and [fix] html and css

--- js/script.js
+++ js/script.js
@@ -11,5 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Ripulisco il contenitore da eventuali elementi statitici
       container.innerHTML = '';
-});
+      // Per ogni oggetto 'picture' creo dinamicamente una colonna + card
+      pictures.forEach(picture => {
+        // Creo l’elemento div che conterrà la singola colonna
+        const col = document.createElement('div');
+        col.className = 'col-12 col-sm-6 col-md-4'; 
+        col.innerHTML = `
+          <div class="card-wrapper">
+            <img src="./img/pin.svg" class="pin" alt="puntina">
+            <div class="card"; position: relative;">
+              <img 
+                src="${picture.url}" 
+                class="photo" 
+                alt="${picture.title || 'Foto'}"
+              >
+              <div class="caption">
+                <p>${picture.title}</p>
+                <time>${picture.date}</time>
+              </div>
+            </div>
+          </div>
+        `;
+        // Inserisco la colonna appena creata dentro la row
+        container.appendChild(col);
+      });
+    })
 });
